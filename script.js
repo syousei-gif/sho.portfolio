76c6d9b5d4c1d6cfe8c086da58ba58e5ce90df1e
syousei-gif <77610482+syousei-gif@users.noreply.github.com>
2022-01-05 00:24:33 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,314 @@
+$(function(){
+
+
+particlesJS("particles-js",{
+	"particles":{
+		"number":{
+			"value":48,//この数値を変更すると幾何学模様の数が増減できる
+			"density":{
+				"enable":true,
+				"value_area":800
+			}
+		},
+		"color":{
+			"value":"#000000"//色
+		},
+		"shape":{
+			"type":"polygon",//形状はpolygonを指定
+			"stroke":{
+				"width":0,
+			},
+	"polygon":{
+		"nb_sides":3//多角形の角の数
+	},
+	"image":{
+		"width":190,
+		"height":100
+	}
+	},
+		"opacity":{
+		"value":0.664994832269074,
+		"random":false,
+		"anim":{
+			"enable":true,
+			"speed":2.2722661797524872,
+			"opacity_min":0.08115236356258881,
+			"sync":false
+		}
+		},
+		"size":{
+			"value":3,
+			"random":true,
+			"anim":{
+				"enable":false,
+				"speed":40,
+				"size_min":0.1,
+				"sync":false
+			}
+		},
+		"line_linked":{
+			"enable":true,
+			"distance":150,
+			"color":"#000000",
+			"opacity":0.6,
+			"width":1
+		},
+		"move":{
+			"enable":true,
+			"speed":6,//この数値を小さくするとゆっくりな動きになる
+			"direction":"none",//方向指定なし
+			"random":false,//動きはランダムにしない
+			"straight":false,//動きをとどめない
+			"out_mode":"out",//画面の外に出るように描写
+			"bounce":false,//跳ね返りなし
+			"attract":{
+				"enable":false,
+				"rotateX":600,
+				"rotateY":961.4383117143238
+			}
+		}
+	},
+	"interactivity":{
+		"detect_on":"canvas",
+		"events":{
+			"onhover":{
+				"enable":false,
+				"mode":"repulse"
+			},
+	"onclick":{
+		"enable":false
+	},
+	"resize":true
+		}
+	},
+	"retina_detect":true
+});
+
+
+
+// ページ内スクロール
+$(function(){
+  var headerHeight = 100; // ヘッダーの高さ
+  $('a[href^="#"]').click(function(){
+      var speed = 500;
+      var href= $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      var position = target.offset().top - headerHeight;
+      $("html, body").animate({scrollTop:position}, speed, "swing");
+      return false;
+  });
+});
+
+
+
+
+
+
+// 動きのきっかけの起点となるアニメーションの名前を定義
+function BgFadeAnime(){
+
+  // 背景色が伸びて出現（右から左）
+$('.bgRLextendTrigger').each(function(){ //bgRLextendTriggerというクラス名が
+  var elemPos = $(this).offset().top-50;//要素より、50px上の
+  var scroll = $(window).scrollTop();
+  var windowHeight = $(window).height();
+  if (scroll >= elemPos - windowHeight){
+    $(this).addClass('bgRLextend');// 画面内に入ったらbgRLextendというクラス名を追記
+  }else{
+    $(this).removeClass('bgRLextend');// 画面外に出たらbgRLextendというクラス名を外す
+  }
+});
+ // 文字列を囲う子要素
+$('.bgappearTrigger').each(function(){ //bgappearTriggerというクラス名が
+  var elemPos = $(this).offset().top-50;//要素より、50px上の
+  var scroll = $(window).scrollTop();
+  var windowHeight = $(window).height();
+  if (scroll >= elemPos - windowHeight){
+    $(this).addClass('bgappear');// 画面内に入ったらbgappearというクラス名を追記
+  }else{
+    $(this).removeClass('bgappear');// 画面外に出たらbgappearというクラス名を外す
+  }
+});		
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function (){
+  BgFadeAnime();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function(){
+  BgFadeAnime();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
+
+
+
+
+
+
+
+
+function slideAnime(){
+	//====下に動くアニメーションここから===
+		$('.downAnime').each(function(){
+            var elemPos = $(this).offset().top-50;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll >= elemPos - windowHeight){
+            // 下から上へ表示するクラスを付与
+            // テキスト要素を挟む親要素（下）とテキスト要素を元位置でアニメーションをおこなう
+            $(this).addClass("slideAnimeDownUp");
+            // 要素を下枠外に移動しCSS アニメーションで下から元の位置に移動
+            $(this).children(".downAnimeInner").addClass("slideAnimeUpDown");
+            // 子要素は親要素のアニメーションに影響されないように逆の指定をし元の位置をキープするアニメーションをおこなう
+            }else{
+            // 下から上へ表示するクラスを取り除く
+            $(this).removeClass("slideAnimeDownUp");
+            $(this).children(".downAnimeInner").removeClass("slideAnimeUpDown");
+            }
+        });
+	}
+	
+	// 画面をスクロールをしたら動かしたい場合の記述
+	$(window).scroll(function (){
+		slideAnime();/* アニメーション用の関数を呼ぶ*/
+	});// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+	// 画面が読み込まれたらすぐに動かしたい場合の記述
+	$(window).on('load', function(){
+		slideAnime();/* アニメーション用の関数を呼ぶ*/
+	});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
+
+
+
+
+  
+
+// blurTriggerにblurというクラス名を付ける定義
+
+function BlurTextAnimeControl() {
+	$('.blurTrigger').each(function(){ //blurTriggerというクラス名が
+		var elemPos = $(this).offset().top-50;//要素より、50px上の
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		if (scroll >= elemPos - windowHeight){
+		$(this).addClass('blur');// 画面内に入ったらblurというクラス名を追記
+		}else{
+		$(this).removeClass('blur');// 画面外に出たらblurというクラス名を外す
+		}
+		});
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+	BlurTextAnimeControl();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+	BlurTextAnimeControl();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
+
+
+
+
+
+
+// 動きのきっかけとなるアニメーションの名前を定義
+function fadeAnime(){
+
+  // ふわっ
+  $('.fadeUpTrigger').each(function(){ //fadeUpTriggerというクラス名が
+    var elemPos = $(this).offset().top-50;//要素より、50px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight){
+    $(this).addClass('fadeUp');// 画面内に入ったらfadeUpというクラス名を追記
+    }else{
+    $(this).removeClass('fadeUp');// 画面外に出たらfadeUpというクラス名を外す
+    }
+    });
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+  $(window).scroll(function (){
+    fadeAnime();/* アニメーション用の関数を呼ぶ*/
+  });// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+  $(window).on('load', function(){
+    fadeAnime();/* アニメーション用の関数を呼ぶ*/
+  });// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
+
+
+
+
+
+
+
+// eachTextAnimeにappeartextというクラス名を付ける定義
+function EachTextAnimeControl() {
+  $('.eachTextAnime').each(function () {
+    var elemPos = $(this).offset().top - 50;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass("appeartext");
+
+    } else {
+      $(this).removeClass("appeartext");
+    }
+  });
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+  EachTextAnimeControl();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+  //spanタグを追加する
+  var element = $(".eachTextAnime");
+  element.each(function () {
+    var text = $(this).text();
+    var textbox = "";
+    text.split('').forEach(function (t, i) {
+      if (t !== " ") {
+        if (i < 10) {
+          textbox += '<span style="animation-delay:.' + i + 's;">' + t + '</span>';
+        } else {
+          var n = i / 10;
+          textbox += '<span style="animation-delay:' + n + 's;">' + t + '</span>';
+        }
+
+      } else {
+        textbox += t;
+      }
+    });
+    $(this).html(textbox);
+  });
+
+  EachTextAnimeControl();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
